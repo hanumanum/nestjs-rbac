6c01bd4e8b9a49004df40e7eaef8cc7768fbb186
hanuman <a.kolyan@gratun.am>
2023-05-04 16:50:39 +0400

    respose hanlers changed

--- src/utils/response.handler.utils.ts
+++ src/utils/response.handler.utils.ts
@@ -1,37 +1,32 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-//TODO: review all this shit
-//TODO: change all code fields to statusCode
 @Injectable()
 export class ResponseHandlerService {
-	private messeges: any = {}
 	private showErrorStack: boolean = false;
 
 	constructor(private readonly configService: ConfigService) {
-		this.messeges = configService.get('messages');
 		this.showErrorStack = (configService.get('environment') === 'development');
 	}
 
 	dataPaginatedHandler = (res, data) => {
 		return res.status(HttpStatus.OK).send({
-			code: HttpStatus.OK,
+			statusCode: HttpStatus.OK,
 			data: data?.data,
 			meta: data.meta
 		});
-
 	}
 
 	dataHandler = (res, data) => {
 		return res.status(HttpStatus.OK).send({
-			code: HttpStatus.OK,
+			statusCode: HttpStatus.OK,
 			data: data
 		});
 	}
 
 	createdHandler = (res, entityTitle: string) => {
 		return res.status(HttpStatus.CREATED).send({
-			code: HttpStatus.CREATED,
+			statusCode: HttpStatus.CREATED,
 			message: `${entityTitle} created`
 		}
 		);
@@ -39,21 +34,15 @@ export class ResponseHandlerService {
 
 	updatedHandler = (res, entityTitle: string) => {
 		return res.status(HttpStatus.OK).send({
-			code: HttpStatus.OK,
+			statusCode: HttpStatus.OK,
 			message: `${entityTitle} updated`
 		});
 	};
 
-	updatedAnswerHandler = (res, message: any, data?: any) => {
-		return res.status(HttpStatus.OK).send({
-			code: HttpStatus.OK,
-			message, data
-		});
-	};
 
 	deletedHandler = (res, entityTitle: string) => {
 		return res.status(HttpStatus.OK).send({
-			code: HttpStatus.OK,
+			statusCode: HttpStatus.OK,
 			message: `${entityTitle} deleted`
 		});
 	};
@@ -68,73 +57,9 @@ export class ResponseHandlerService {
 	errorHandler = (res, err: Error, message = "") => {
 		return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
 			message: `${err.message} \n ${message}`,
-			code: HttpStatus.INTERNAL_SERVER_ERROR,
+			statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
 			error: (this.showErrorStack) ? err.stack : ""
 		});
 	};
 
-	noUserHandler = (res) => {
-		return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-			code: HttpStatus.INTERNAL_SERVER_ERROR,
-			message: this.messeges.noSuchUser
-		});
-	};
-
-	syncHandler = (res, what, isSuccess: number) => {
-		const statusCode = isSuccess === 1 ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR;
-		return res.status(statusCode).send({
-			code: statusCode,
-			message: isSuccess ? `${what} ${this.messeges.syncedSuccessfully}` : `${this.messeges.cannotSync} ${what}`
-		});
-	};
-
-	alreadySubmittedHandler = (res) => {
-		return res.status(298).send({
-			code: 298,
-			message: this.messeges.alreadyAnswered
-		});
-	};
-
-	appointmentCanceledHandler = (res) => {
-		return res.status(297).send({
-			code: 297,
-			message: this.messeges.cancaledAppointment
-		});
-	};
-
-	uuidTokenExpiredHandler = (res) => {
-		return res.status(299).send({
-			code: 299,
-			message: this.messeges.tokenExpired
-		});
-	};
-
-	telNumberRequiredHandler = (res) => {
-		return res.status(400).send({
-			code: 400,
-			message: this.messeges.phoneRequired
-		});
-	}
-
-	noQuestinnaireHandler = (res) => {
-		return res.status(499).send({
-			code: 499,
-			message: this.messeges.noquestionnaire
-		})
-
-	}
-
-	checkYourPhoneHandler = (res) => {
-		return res.status(HttpStatus.OK).send({
-			code: HttpStatus.OK,
-			message: this.messeges.checkYourPhone
-		})
-	}
-
-	successHandler = (res, message) => {
-		return res.status(HttpStatus.OK).send({
-			code: HttpStatus.OK,
-			message: message
-		})
-	}
 }
